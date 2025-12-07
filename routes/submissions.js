aa373f544898cb7d9a0357b6e7c768a2ed36c900
RuanKelvin29 <tuvip299@gmail.com>
2025-12-08 00:16:33 +0700

    add fs

--- routes/submissions.js
+++ routes/submissions.js
@@ -2,6 +2,7 @@ const express = require("express");
 const sql = require("mssql");
 const router = express.Router();
 const path = require("path");
+const fs = require("fs");
 const authenticateToken = require('../middleware/auth');
 const authorizeRole = require('../middleware/authorizeRole');
 const { scoreWriting, scoreSpeaking } = require('../ai/aiService');
